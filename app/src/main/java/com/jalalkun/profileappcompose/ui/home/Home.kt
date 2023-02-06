43192a6c9ce497c2828c5ea08c5cec6adb520e4f
jalalkun <jalaludin.karim@gmail.com>
2023-02-07 00:13:48 +0700

    improve list scroll

--- app/src/main/java/com/jalalkun/profileappcompose/ui/home/Home.kt
+++ app/src/main/java/com/jalalkun/profileappcompose/ui/home/Home.kt
@@ -2,12 +2,13 @@ package com.jalalkun.profileappcompose.ui.home
 
 import android.util.Log
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
 import androidx.navigation.NavHostController
 import com.jalalkun.profileappcompose.data.model.DataProfile
 import com.jalalkun.profileappcompose.data.model.DetailProfile
@@ -18,6 +19,7 @@ import com.jalalkun.profileappcompose.utils.isScrolledToTheEnd
 import com.jalalkun.profileappcompose.widget.DataNotFound
 import com.jalalkun.profileappcompose.widget.ErrorAlert
 import com.jalalkun.profileappcompose.widget.Loading
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 import java.net.URLEncoder
@@ -39,24 +41,26 @@ fun HomeScreen(
                 profileViewModel.dismissError()
             }
         }
+
         is Resource.Success<*> -> {
             profileViewModel.success()
-            val result = listProfile.data as List<DataProfile>
+            val result = listProfile.data as List<*>
             result.forEach {
-                if (!profileViewModel.dataProfiles.contains(it)) {
+                if (it is DataProfile &&  !profileViewModel.dataProfiles.contains(it) && it.id?.value != null) {
                     profileViewModel.dataProfiles.add(it)
                 }
             }
         }
+
         is Resource.Loading -> {
             Loading()
         }
+
         else -> {
             DataNotFound()
         }
     }
     HomeContent(
-        list = profileViewModel.dataProfiles,
         profileViewModel = profileViewModel,
         navController = navController
     )
@@ -64,46 +68,60 @@ fun HomeScreen(
 
 @Composable
 private fun HomeContent(
-    list: List<DataProfile>,
     profileViewModel: ProfileViewModel,
     navController: NavHostController
 ) {
+    val list = remember {
+        profileViewModel.dataProfiles
+    }
+    val coroutine = rememberCoroutineScope()
     val lazyListState = rememberLazyListState()
     if (list.isEmpty()) DataNotFound()
     else {
         LazyColumn(
             content = {
-                items(list) { profile ->
+                items(
+                    count = list.size,
+                    key = {
+                        list[it].id?.name + list[it].id?.value
+                    }
+                ) {
+                    val profile = list[it]
                     CardProfile(dataProfile = profile) {
                         Log.e("MainNavigation", "toDetailProfile: ")
-                        val detailProfile =
-                            DetailProfile(
-                                name = "%s, %s %s".format(
-                                    profile.name?.title,
-                                    profile.name?.first,
-                                    profile.name?.last
-                                ),
-                                phoneNumber = profile.phone.toString(),
-                                email = profile.email.toString(),
-                                address = "%s %s %s".format(
-                                    profile.location?.street,
-                                    profile.location?.city,
-                                    profile.location?.state
-                                ),
-                                birthday = profile.dob?.date.toString(),
-                                picture = URLEncoder.encode(
-                                    profile.picture?.large.toString(),
-                                    StandardCharsets.UTF_8.toString()
+                        coroutine.launch(Dispatchers.IO) {
+                            val detailProfile =
+                                DetailProfile(
+                                    name = "%s, %s %s".format(
+                                        profile.name?.title,
+                                        profile.name?.first,
+                                        profile.name?.last
+                                    ),
+                                    phoneNumber = profile.phone.toString(),
+                                    email = profile.email.toString(),
+                                    address = "%s %s %s".format(
+                                        profile.location?.street,
+                                        profile.location?.city,
+                                        profile.location?.state
+                                    ),
+                                    birthday = profile.dob?.date.toString(),
+                                    picture = URLEncoder.encode(
+                                        profile.picture?.large.toString(),
+                                        StandardCharsets.UTF_8.toString()
+                                    )
                                 )
-                            )
-                        navController.toDetailProfile(detailProfile)
+                            navController.toDetailProfile(detailProfile)
+                        }
                     }
                 }
             },
-            state = lazyListState
+            state = lazyListState,
         )
-        if (lazyListState.isScrollInProgress && lazyListState.isScrolledToTheEnd()) {
-            profileViewModel.getProfiles()
-        }
+    }
+    LaunchedEffect(Unit) {
+        snapshotFlow { lazyListState.isScrolledToTheEnd() }
+            .collect {
+                if (it && list.isNotEmpty()) profileViewModel.getProfiles()
+            }
     }
 }
